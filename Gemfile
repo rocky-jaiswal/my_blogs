8bcf0572009bbe099932e6f9799f5c7b34af4783
Rocky Jaiswal <rocky.jaiswal@gmail.com>
2013-07-13 11:43:53 +0530

    Adding capistrano
    Signed-off-by: Rocky Jaiswal <rocky.jaiswal@gmail.com>

--- Gemfile
+++ Gemfile
@@ -4,6 +4,6 @@ gem "middleman"
 gem "middleman-blog"
 gem "nokogiri"
 gem "slim"
-
+gem "capistrano"
 # For feed.xml.builder
 gem "builder"
---
+++ config/deploy.rb
@@ -0,0 +1,15 @@
+set :application, "my_blogs"
+set :repository,  "git@github.com:rocky-jaiswal/my_blogs.git"
+
+set  :scm, :git
+set  :user, "deploy"
+set  :use_sudo, false
+set  :deploy_via, :remote_cache
+set  :branch, "master"
+set  :deploy_to, "/home/#{user}/my_blogs"
+role :web, "198.211.98.21"                          # Your HTTP server, Apache/etc
+role :app, "198.211.98.21"                          # This may be the same as your `Web` server
+role :db,  "198.211.98.21", :primary => true # This is where Rails migrations will run
+
+# if you want to clean up old releases on each deploy uncomment this:
+after "deploy:restart", "deploy:cleanup"
