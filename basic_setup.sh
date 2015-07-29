5b067d68f5de71126fccb4f6d624bf65f2303424
rockyj <rocky.jaiswal@gmail.com>
2015-07-29 23:36:54 +0200

    minor change

--- basic_setup.sh
+++ basic_setup.sh
@@ -4,6 +4,11 @@ apt-get update
 apt-get upgrade
 
 apt-get install -y fail2ban
+apt-get install -y ufw
+ufw allow 22
+ufw allow 80
+ufw allow 443
+ufw enable
 
 useradd deploy
 passwd deploy
@@ -15,12 +20,6 @@ mkdir /home/deploy/.ssh
 chmod 700 /home/deploy/.ssh
 chown -R deploy /home/deploy
 chown -R deploy /home/deploy/.ssh
-
-apt-get install -y ufw
-ufw allow 22
-ufw allow 80
-ufw allow 443
-ufw enable
 
 useradd rockyj
 passwd  rockyj
