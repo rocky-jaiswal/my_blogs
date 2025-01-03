ca4fa4a5e3d2409b15b1509696f0ea5d91181e89
Rocky Jaiswal <rocky.jaiswal@gmail.com>
2025-01-03 19:15:06 +0100

    more cleanup

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -3,8 +3,13 @@ import type { CollectionEntry } from "astro:content";
 import { parse, compareDesc } from "date-fns";
 import { getCollection } from "astro:content";
 
+const DATE_FORMAT = "dd/MM/yyyy";
+
+export const toDate = (date: string) => parse(date, DATE_FORMAT, new Date());
+
 export const postToUrlComponents = (post: CollectionEntry<"blog">) => {
-  const date = parse(post.data.date, "dd/MM/yyyy", new Date());
+  const date = toDate(post.data.date);
+
   const year = date.getFullYear();
   const month = date.getMonth() + 1;
   const datex = date.getDate();
@@ -25,9 +30,6 @@ export const sortedPosts = async () => {
   const posts = await getCollection("blog");
 
   return posts.sort((a, b) =>
-    compareDesc(
-      parse(a.data.date, "dd/MM/yyyy", new Date()),
-      parse(b.data.date, "dd/MM/yyyy", new Date())
-    )
+    compareDesc(toDate(a.data.date), toDate(b.data.date))
   );
 };
